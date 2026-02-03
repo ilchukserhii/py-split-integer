d21d7b11f2609b2c6b1025c0e1192eb4c34af5d4
Seroga <ilchuk.cergey@gmail.com>
2026-02-03 14:38:54 +0200

    solution3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert len(split_integer(6, 2)) == 2
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -18,7 +18,7 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert 0 in split_integer(2, 4)
+    assert split_integer(2, 4) == [0, 0, 1, 1]
 
 
 def test_only_last_number_is_increment() -> None:
@@ -26,4 +26,5 @@ def test_only_last_number_is_increment() -> None:
 
 
 def test_max_and_min_result_should_be_not_greater_than_one() -> None:
-    assert max(split_integer(8, 1)) - min(split_integer(8, 1)) <= 1
+    result = split_integer(17, 4)
+    assert max(result) - min(result) <= 1
